1a3462a60341610ad3a7bd4d5e1f7bd2e0026ecb
joaaosc <joaocosta29@poli.ufrj.br>
2025-12-23 20:11:38 -0300

    add: endpoint GET /health

--- Sources/GeoHub/routes.swift
+++ Sources/GeoHub/routes.swift
@@ -1,11 +1,15 @@
 import Vapor
 
-func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
+struct HealthResponse: Content {
+    let status: String
+    let timestamp: String
+}
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+func routes(_ app: Application) throws {
+    app.get("health") { req async -> HealthResponse in
+        HealthResponse(
+            status: "ok",
+            timestamp: ISO8601DateFormatter().string(from: Date())
+        )
     }
 }
